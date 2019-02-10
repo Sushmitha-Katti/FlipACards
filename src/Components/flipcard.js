288cb8ac8140b6106cf3550766a242de9d78798d
Sushmitha Katti <sushmitha.katti.19@gmail.com>
2019-02-10 13:03:58 +0530

    Added media query

--- src/Components/flipcard.js
+++ src/Components/flipcard.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
 import styled, { keyframes } from "styled-components";
 
-import brand from "./Images/brand.png";
-import branding from "./Images/branding.png";
-import business from "./Images/business.jpg";
-import machinelearning from "./Images/machinelearning.png";
-import marketing from "./Images/marketing.png";
-import website from "./Images/website.png";
-
 /*------------------STYLED COMPONENTS------------*/
 
 /*WRAPPER CLASS*/
@@ -17,12 +10,9 @@ const FlipCardWrapper = styled.div.attrs(({ padding, bg, headimg }) => ({
   bg: bg,
   headimg: headimg
 }))`{
-
-
   background:#ccc;
   padding: 50px;
   font-family: "Nunito", sans-serif;
-
   .flip-card {
     background-color: transparent;
     width: 340px;
@@ -31,20 +21,19 @@ const FlipCardWrapper = styled.div.attrs(({ padding, bg, headimg }) => ({
     -moz-perspective: 150rem;
     border-radius: 3%;
   }
-
   .flip-card-inner {
     position: relative;
     width: 100%;
     height: 100%;
-
     transition: transform 0.8s;
     transform-style: preserve-3d;
   }
 
-  .flip-card:hover .flip-card-inner {   /*For Flipping Card while Hovered */
-    transform: rotateY(-180deg);
+  @media only screen and (min-width:56.25em) {
+    .flip-card:hover .flip-card-inner {   /*For Flipping Card while Hovered */
+      transform: rotateY(-180deg);
+    }
   }
-
   .flip-card-front,   /* General character for both faces of card*/
   .flip-card-back {
     position: absolute;
@@ -52,15 +41,11 @@ const FlipCardWrapper = styled.div.attrs(({ padding, bg, headimg }) => ({
     height: 100%;
     backface-visibility: hidden;
   }
-
   .flip-card-front {        /*Front face */
     background-color: white;
     
     z-index: 1;
-
-
     img {                /*image of card */
-
       background-size: cover;
       width: 100%;
       height: 50%;
@@ -80,7 +65,6 @@ const FlipCardWrapper = styled.div.attrs(({ padding, bg, headimg }) => ({
       position: relative;
       opadding-inline-start: 20px;
       line-height : 1.7
-
       ul { 
         color:#777;
         font-weight:200;
@@ -96,8 +80,6 @@ const FlipCardWrapper = styled.div.attrs(({ padding, bg, headimg }) => ({
     top: 7.5rem;
     right: 1.2rem;
     width: 75%;
-
-
     span{   /* card heading background and text */
       background-image: ${props => props.headimg};
       padding: ${props => props.padding};
@@ -108,14 +90,8 @@ const FlipCardWrapper = styled.div.attrs(({ padding, bg, headimg }) => ({
     
     }
   }
-
   }  /* End Of Front face*/
-
-
-
-
   .flip-card-back {                       /*BACK FACE */
-
     background-image: ${props => props.bg};
     color: white;
     text-align: center;
@@ -124,8 +100,8 @@ const FlipCardWrapper = styled.div.attrs(({ padding, bg, headimg }) => ({
     margin:0 auto;
     position: relative;
 
-    .back-card-details{   /* Back Card Details including Button and text */
 
+    .back-card-details{   /* Back Card Details including Button and text */
       position:absolute;
       top: 50%;
       left: 50%;
@@ -133,33 +109,92 @@ const FlipCardWrapper = styled.div.attrs(({ padding, bg, headimg }) => ({
       transform: translate(-50%,-50%);
       width: 90%;
       text-align: center
-
       .about{        /* Back Card --- Only Text*/
         text-align: center;
         color: #fff;
         margin-bottom: 5.5rem;
-
         .about-general {  /*First Line Of Card */
           font-size: 1rem;
           text-transform: uppercase;
           margin: 0
         }
-
         .about-specific { /* Second Line Of Card */
           font-size: 2.8rem;
           line-height: 2.5rem;
           font-weight: 50;
           margin: 0px;
         
-
         }
       }
     }
   }
 }
 }
+
+@media only screen and (max-width:56.25em){
+
+  .flip-card {
+    height: 28rem;
+    width: 22.5rem;    
+  }
+  .flip-card-inner {
+    
+    position: relative;
+    box-shadow: none;
+  }
+  .flip-card-front img {
+    height: 50%;
+    
+  }
+  .flip-card-front .card-heading {
+    
+    top: 1rem;
+
+    span {  
+      font-size: 1.2rem;
+    }
+  }
+  .card-details {
+    padding: 0.1rem;
+
+    ul{
+      font-size: 0.9rem;
+    }
+ }
+ 
+   .flip-card-back {
+     top:85%;
+     height:15%;
+    -webkit-transform: rotateY(0deg);
+    transform: rotateY(0deg);
+    webkit-clip-path: polygon(0 15%,100% 0,100% 100%,0 100%);
+
+    clip-path: polygon(0 15%,100% 0,100% 100%,0 100%);
+    
+
+}
+.flip-card-back .back-card-details {
+  top:20%;
+  left: 30%;
 }
 
+.about {
+  position: absolute;
+  top: 0;
+  text-align: center;
+  width: 100%;
+  padding: 2rem
+}
+.about-general, .about-specific  {
+  display : none;
+}
+
+ 
+}
+
+
+
+}
 `;
 
 const buttonani = keyframes` {
@@ -214,13 +249,11 @@ const Button = styled.div`
   border: none;
   cursor: pointer;
   margin: 0.3rem;
-
   a{
     color: #777;
     text-decoration: none;
     
   }
-
   :hover {
     animation: ${buttonani} 0.2s linear forwards;
     -webkit-animation-fill-mode: forwards; 
@@ -229,6 +262,12 @@ const Button = styled.div`
   }
   animation: ${afterbutton} 0.2s ease-in forwards;
    
+}
+@media only screen and (max-width:56.25em) {
+  padding: 0.6rem 1.2rem;
+  font-size:0.9rem;
+  top:1.3rem;
+  left:4.5rem;;
 }
   
 `;
@@ -267,7 +306,6 @@ class FlipCard extends Component {
                       {this.props.parameters.backSecond}
                     </p>
                   </div>
-
                   <Button>
                     <a>Tell Me More</a>
                   </Button>
